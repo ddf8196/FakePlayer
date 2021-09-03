defb497d956a0672d9b9fc2df982cc10ba4b2926
Jasonzyt <2126931871@qq.com>
2021-09-03 22:57:25 +0800

    Add JS API: `Script.log(...)` Modify DeviceID

--- src/main/java/com/ddf/fakeplayer/js/ScriptAPI.java
+++ src/main/java/com/ddf/fakeplayer/js/ScriptAPI.java
@@ -1,14 +1,21 @@
 package com.ddf.fakeplayer.js;
 
 import com.ddf.fakeplayer.actor.player.FakePlayer;
+import com.ddf.fakeplayer.util.Logger;
 
 public class ScriptAPI {
     private final PlayerScript playerScript;
     private final FakePlayer player;
+    private final Logger logger;
 
     ScriptAPI(PlayerScript playerScript, FakePlayer player) {
         this.playerScript = playerScript;
         this.player = player;
+        this.logger = Logger.getLogger();
+    }
+
+    public void log(Object... log) {
+        logger.log(log);
     }
 
     public FakePlayer getPlayer() {
