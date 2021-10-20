49cc6f28e734bcd9f7689b1784cfb48e71fe29d1
xiaoqch <xiaoqch@foxmail.com>
2021-10-20 10:12:06 +0800

    Update protocol library to v471 to support bds 1.17.40

--- src/main/java/com/ddf/fakeplayer/util/ProtocolVersionUtil.java
+++ src/main/java/com/ddf/fakeplayer/util/ProtocolVersionUtil.java
@@ -19,6 +19,7 @@ import com.nukkitx.protocol.bedrock.v431.Bedrock_v431;
 import com.nukkitx.protocol.bedrock.v440.Bedrock_v440;
 import com.nukkitx.protocol.bedrock.v448.Bedrock_v448;
 import com.nukkitx.protocol.bedrock.v465.Bedrock_v465;
+import com.nukkitx.protocol.bedrock.v471.Bedrock_v471;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -47,6 +48,7 @@ public class ProtocolVersionUtil {
         registerPacketCodec(Bedrock_v440.V440_CODEC, 10);
         registerPacketCodec(Bedrock_v448.V448_CODEC, 10);
         registerPacketCodec(Bedrock_v465.V465_CODEC, 10);
+        registerPacketCodec(Bedrock_v471.V471_CODEC, 10);
         codecMap = Collections.unmodifiableMap(codecMap);
         rakNetVersionMap = Collections.unmodifiableMap(rakNetVersionMap);
     }
