0ceff2207415773c9f4293397f961844bd9862dd
Jasonzyt <2126931871@qq.com>
2022-04-22 22:54:33 +0800

    Update protocol to v503
    Support 1.18.30

--- src/main/java/com/ddf/fakeplayer/util/ProtocolVersionUtil.java
+++ src/main/java/com/ddf/fakeplayer/util/ProtocolVersionUtil.java
@@ -20,6 +20,7 @@ import com.nukkitx.protocol.bedrock.v465.Bedrock_v465;
 import com.nukkitx.protocol.bedrock.v471.Bedrock_v471;
 import com.nukkitx.protocol.bedrock.v475.Bedrock_v475;
 import com.nukkitx.protocol.bedrock.v486.Bedrock_v486;
+import com.nukkitx.protocol.bedrock.v503.Bedrock_v503;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -56,6 +57,7 @@ public class ProtocolVersionUtil {
         registerPacketCodec(Bedrock_v471.V471_CODEC, 10, "blockpalette_1.17.41.01_v471.nbt");
         registerPacketCodec(Bedrock_v475.V475_CODEC, 10, "blockpalette_1.18.2.03_v475.nbt");
         registerPacketCodec(Bedrock_v486.V486_CODEC, 10, null);
+        registerPacketCodec(Bedrock_v503.V503_CODEC, 10, null);
         codecMap = Collections.unmodifiableMap(codecMap);
         rakNetVersionMap = Collections.unmodifiableMap(rakNetVersionMap);
         blockPaletteMap = Collections.unmodifiableMap(blockPaletteMap);
