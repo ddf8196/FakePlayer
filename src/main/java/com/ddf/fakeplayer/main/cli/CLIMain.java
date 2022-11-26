f9af444bb7558ac4c246c38a15a8ab507534210f
yqs112358 <yqs112358@outlook.com>
2022-11-26 18:13:55 +0800

    add connect & disconnect command

--- src/main/java/com/ddf/fakeplayer/main/cli/CLIMain.java
+++ src/main/java/com/ddf/fakeplayer/main/cli/CLIMain.java
@@ -84,12 +84,14 @@ public class CLIMain extends Main {
                 case "?":
                 case "help":
                     logger.log("add 假人名称 - 添加一个假人");
+                    logger.log("connect 假人名称 - 上线一个假人");
+                    logger.log("disconnect 假人名称 - 下线一个假人");
                     logger.log("remove 假人名称 - 移除一个假人");
                     logger.log("list - 列出所有假人");
-                    logger.log("publicKey - 显示当前使用的公钥");
                     logger.log("stop - 断开所有连接并停止运行");
                     logger.log("enableWebSocket - 启用WebSocket");
                     logger.log("disableWebSocket - 禁用WebSocket");
+                    logger.log("publicKey - 显示当前使用的公钥");
                     logger.log("help - 显示此帮助信息");
                     break;
                 case "add":
@@ -105,9 +107,37 @@ public class CLIMain extends Main {
                         removePlayer(playerName);
                     }
                     break;
+                case "connect":
+                    if(string.length >= 2){
+                        String playerName = string[1];
+                        Client client = getClient(playerName);
+                        if (client == null)
+                            logger.log(playerName + " 未找到");
+                        else if (client.isConnected())
+                            logger.log(playerName + " 已在线");
+                        else {
+                            client.connect(getConfig().getServerAddress(), getConfig().getServerPort());
+                            logger.log(playerName + " 上线成功");
+                        }
+                    }
+                    break;
+                case "disconnect":
+                    if(string.length >= 2) {
+                        String playerName = string[1];
+                        Client client = getClient(playerName);
+                        if (client == null)
+                            logger.log(playerName + " 未找到");
+                        else if (!client.isConnected())
+                            logger.log(playerName + " 已离线");
+                        else {
+                            client.stop();
+                            logger.log(playerName + " 下线成功");
+                        }
+                    }
+                    break;
                 case "list":
                     logger.log("目前共有 ", clients.size(), " 个假人: ");
-                    clients.forEach(client -> logger.log(client.getPlayerName()));
+                    clients.forEach(client -> logger.log(client.getPlayerName() + " - " + (client.isConnected() ? "在线" : "离线")));
                     break;
                 case "publicKey":
                     logger.log(config.getServerPublicKey());
